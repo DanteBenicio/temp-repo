07f657b2485dace0d7863f66cb80e34e4d1d3fd2
DanteBenicio <dantecosta79@gmail.com>
2021-08-02 14:32:49 -0300

    :fire: Comments removed

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -91,25 +91,4 @@ class Home extends Component {
   }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-        
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default Home;
