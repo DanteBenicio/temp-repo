94536fb7019344f36bec577cc74de24d28e7ac0c
DanteBenicio <dantecosta79@gmail.com>
2021-08-02 12:33:54 -0300

    :sparkles: Create Posts component

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+import React from 'react'
+import PostCard from '../PostCard'
+
+const Posts = ({ posts }) => (
+  <div className="posts">
+    {posts.map(post => (
+      <PostCard post={post} key={post.id}/>
+    ))}
+  </div>
+)
+
+export default Posts
