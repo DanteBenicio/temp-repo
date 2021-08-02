542f6c5e3364360fabd9c8550dfc861fc9f6a30c
DanteBenicio <dantecosta79@gmail.com>
2021-08-02 12:30:55 -0300

    :sparkles: Create PostCard component

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css'
+import React from 'react'
+import PostImages from '../PostImages';
+
+const PostCard = ({ post }) => (
+    <div className="post">
+      <PostImages post={post}/>
+
+      <div key={post.id} className="post-content">
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+    </div>
+)
+
+export default PostCard;
